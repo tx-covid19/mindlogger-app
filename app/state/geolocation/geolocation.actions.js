de3beb8bb1f1645b12807f34a86e5989fee450c7
Henry Rossiter <henry.rossiter@utexas.edu>
2020-05-07 07:29:37 -0500

    clear geolocation action

--- app/state/geolocation/geolocation.actions.js
+++ app/state/geolocation/geolocation.actions.js
@@ -1,5 +1,9 @@
 import GEOLOCATION_CONSTANTS from './geolocation.constants';
 
+export const clearGeolocationData = () => ({
+  type: GEOLOCATION_CONSTANTS.CLEAR,
+});
+
 export const setGeolocationData = info => ({
   type: GEOLOCATION_CONSTANTS.SET_DATA,
   payload: info,
