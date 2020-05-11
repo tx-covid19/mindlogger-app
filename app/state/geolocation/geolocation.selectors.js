b7e5b3ea98d03494379dd2c7045c4ac4e688c597
Henry Rossiter <henry.rossiter@utexas.edu>
2020-05-11 15:01:09 -0500

    modify geolocation store shape  for statistics

--- app/state/geolocation/geolocation.selectors.js
+++ app/state/geolocation/geolocation.selectors.js
@@ -1,3 +1,7 @@
 import * as R from 'ramda';
 
 export const dataSelector = R.path(['geolocation', 'data']);
+
+export const statisticsSelector = R.path(['geolocation', 'statistics']);
+
+export const statisticsLastSentSelector = R.path(['geolocation', 'statisticsLastSent']);
